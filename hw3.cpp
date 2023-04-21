842f3145d0879ab4fdefcc576e693495ef37491f
Kevin Jarquin <92592475+KevJar143@users.noreply.github.com>
2023-04-21 12:43:34 -0500

    Added Names to var

--- hw3.cpp
+++ hw3.cpp
@@ -7,8 +7,6 @@
 using namespace std;
 
 vector<int> recursiveBestApplicant(const vector<pair<float, float>>& applicants, int start, int end) {
-
-    // Base Case
     if (start >= end) {
         return {};
     }
@@ -24,27 +22,36 @@ vector<int> recursiveBestApplicant(const vector<pair<float, float>>& applicants,
 
     set<int> eligibleCandidates;
 
-    // Inserts eligible candidates into the set.
+    // Compares leftBest and rightBest applicants
     for (const auto& leftIndex : leftBest) {
         bool isBest = true;
         for (const auto& rightIndex : rightBest) {
-            if (applicants[rightIndex].first > applicants[leftIndex].first &&
-                applicants[rightIndex].second < applicants[leftIndex].second) {
+            float leftWPM = applicants[leftIndex].first;
+            float leftIPM = applicants[leftIndex].second;
+            float rightWPM = applicants[rightIndex].first;
+            float rightIPM = applicants[rightIndex].second;
+
+            if (rightWPM > leftWPM && rightIPM < leftIPM) {
                 isBest = false;
                 break;
             }
         }
+        // Inserts eligible candidates into the set.
         if (isBest) {
             eligibleCandidates.insert(leftIndex);
         }
     }
 
-    // Compares rightBest and leftBest applicants, checks eligibility
+    // Compares rightBest and leftBest applicants
     for (const auto& rightIndex : rightBest) {
         bool isBest = true;
         for (const auto& leftIndex : leftBest) {
-            if (applicants[leftIndex].first > applicants[rightIndex].first &&
-                applicants[leftIndex].second < applicants[rightIndex].second) {
+            float leftWPM = applicants[leftIndex].first;
+            float leftIPM = applicants[leftIndex].second;
+            float rightWPM = applicants[rightIndex].first;
+            float rightIPM = applicants[rightIndex].second;
+
+            if (leftWPM > rightWPM && leftIPM < rightIPM) {
                 isBest = false;
                 break;
             }
@@ -57,6 +64,7 @@ vector<int> recursiveBestApplicant(const vector<pair<float, float>>& applicants,
 
     return vector<int>(eligibleCandidates.begin(), eligibleCandidates.end());
 }
+
 
 vector<int> BestApplicants(const vector<pair<float, float>>& applicants) {
     return recursiveBestApplicant(applicants, 0, applicants.size());
